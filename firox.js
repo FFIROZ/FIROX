cfd36cda7a2543f21121b66c3d8104c1e5d88696
FFIROZX <91351092+FFIROZX@users.noreply.github.com>
2021-09-27 04:06:50 -0500

    Update firox.js

--- firox.js
+++ firox.js
@@ -1,26 +1,178 @@
-const { WAConnection } = require('@adiwajshing/baileys');
+const { WAConnection, MessageType, Mimetype, } = require('@adiwajshing/baileys');
 const fs = require('fs');
+const prefix = '.'
 
 async function iniciar () { 
         const client = new WAConnection()
-
+//Aquí el "client" lo pueden cambiar a su gusto. Pero si cambian, tendrán que cambiar todos los "client" por el cambio que hicieron.
         client.logger.level = 'warn'
 
+//llamar al código QR
         client.on('qr', () => {
         })
 
-        fs.existsSync('./package.json') && client.loadAuthInfo('./package.json')
+//crear un archivo Json para guardar información: ID del cliente, Token y Keys del cliente y del SERVER.
+        fs.existsSync('./firox.json') && client.loadAuthInfo('./firox.json')
 
+//Conectando o reconectando
         client.on('connecting', () => {
         console.log('Conectando')
         })
 
+//La conexión fue en éxito👌🏻
         client.on('open', () => {
         console.log('Conectado exitosamente :D')
         })
         await client.connect({timeoutMs: 30*1000})
-        fs.writeFileSync('./package.json', JSON.stringify(client.base64EncodedAuthInfo(), null, '\t'))
-        }
+        fs.writeFileSync('./firox.json', JSON.stringify(client.base64EncodedAuthInfo(), null, '\t'))
+        
 
+client.on('chat-update', async (sam) => {
+try {	  
+if (!sam.hasNewMessage) return
+if (!sam.messages) return
+if (sam.key && sam.key.remoteJid == 'status@broadcast') return
+
+sam = sam.messages.all()[0]
+if (!sam.message) return
+global.blocked
+sam.message = (Object.keys(sam.message)[0] === 'ephemeralMessage') ? sam.message.ephemeralMessage.message : sam.message
+const from = sam.key.remoteJid
+const type = Object.keys(sam.message)[0]        
+const quoted = type == 'extendedTextMessage' && sam.message.extendedTextMessage.contextInfo != null ? sam.message.extendedTextMessage.contextInfo.quotedMessage || [] : []
+const typeQuoted = Object.keys(quoted)[0]
+const content = JSON.stringify(sam.message)
+const { text, extendedText, contact, location, liveLocation, image, video, sticker, document, audio, product } = MessageType
+const body = sam.message.conversation || sam.message[type].caption || sam.message[type].text || ""
+chats = (type === 'conversation') ? sam.message.conversation : (type === 'extendedTextMessage') ? sam.message.extendedTextMessage.text : ''
+budy = (type === 'conversation' && sam.message.conversation.startsWith(prefix)) ? sam.message.conversation : (type == 'imageMessage') && sam.message.imageMessage.caption.startsWith(prefix) ? sam.message.imageMessage.caption : (type == 'videoMessage') && sam.message.videoMessage.caption.startsWith(prefix) ? sam.message.videoMessage.caption : (type == 'extendedTextMessage') && sam.message.extendedTextMessage.text.startsWith(prefix) ? sam.message.extendedTextMessage.text : ''
+
+if (prefix != "") {
+if (!body.startsWith(prefix)) {
+cmd = false
+comm = ""
+} else {
+cmd = true
+comm = body.slice(1).trim().split(" ").shift().toLowerCase()
+}
+} else {
+cmd = false
+comm = body.trim().split(" ").shift().toLowerCase()
+}
+        
+const command = comm
+
+const arg = chats.slice(command.length + 2, chats.length)
+const args = budy.trim().split(/ +/).slice(1)
+const isCmd = budy.startsWith(prefix)
+const q = args.join(' ')
+const soyYo = client.user.jid
+const botNumber = client.user.jid.split("@")[0]
+const ownerNumber = ['########@s.whatsapp.net']
+const isGroup = from.endsWith('@g.us')
+const sender = sam.key.fromMe ? client.user.jid : isGroup ? sam.participant : sam.key.remoteJid
+const senderNumber = sender.split("@")[0]
+const isMe = senderNumber == botNumber
+const conts = sam.key.fromMe ? client.user.jid : client.contacts[sender] || { notify: jid.replace(/@.+/, '') }
+const pushname = sam.key.fromMe ? client.user.name : conts.notify || conts.vname || conts.name || '-'
+
+
+const boton = (para, contenido, footer, botones = [], samu330 = {}) => {
+const buttonMessage = {
+contentText: contenido,
+footerText: footer,
+buttons: botones,
+headerType: 1
+}
+client.sendMessage(para, buttonMessage, MessageType.buttonsMessage, samu330)
+}
+
+
+if(body.includes('bot')) {
+client.sendMessage(from, 'Hola!', MessageType.text, {quoted: { key: {
+fromMe: false,
+participant: `0@s.whatsapp.net`, ...(from ? { remoteJid: "status@broadcast" } : {})
+},
+message: {
+"documentMessage": { "title": "Sam🍒", 'jpegThumbnail': fs.readFileSync('./media/logo.jpg')}}
+}})
+}
+        
+if(body == ('hola')) {
+client.sendMessage(from, 'como estas!', MessageType.text, {quoted: sam})
+}
+switch (command) {
+
+case 'bot':
+client.sendMessage(from, 'Hola, felicidades, has logrado enviar un mensaje mediante un servidor externo😚', text, {quoted: { key: {
+fromMe: false,
+participant: `0@s.whatsapp.net`, ...(from ? { remoteJid: "status@broadcast" } : {})
+},
+message: {
+"documentMessage": { "title": "Samu330🍒", 'jpegThumbnail': fs.readFileSync('./media/logo.jpg')}}
+}})
+break
+                
+case 'foto':
+const imagen = fs.readFileSync('./media/logo.jpg')                
+client.sendMessage(from, imagen, MessageType.image, {quoted: sam, caption: `*Bien, has enviado una foto con caption!*`})
+break
+                
+case 'video':
+const video = fs.readFileSync('./media/video.mp4')
+client.sendMessage(from, video, MessageType.video, {quoted: sam, mimetype: 'video/mp4', caption: 'JAJAJA', duration: 999999999})
+break
+                
+case 'audio':
+const audio = fs.readFileSync('./media/audio.mp3')
+client.sendMessage(from, audio, MessageType.audio, {quoted: sam, mimetype: 'audio/mp3', duration: -9999999, ptt: true})
+client.sendMessage(from, audio, MessageType.audio, {quoted: sam, mimetype: 'audio/mp3', duration: -9999999})                
+break
+                
+case 'botones':
+boton(from, 'Hola', `${pushname}`, [{buttonId: 'b1', buttonText: {displayText: 'Click Aqui'}, type: 1}])           
+break
+                
+case 'lista':
+let lista = client.prepareMessageFromContent(from,{
+"listMessage": {
+"title": `${pushname}`,
+"description": `Este es un Mensaje de lista`,
+"buttonText": "Click Aqui",
+"listType": "SINGLE_SELECT",
+"sections": [
+{ "title": `Seccion 1`,
+"rows": [
+{
+"title": 'Lista 1',
+"description": 'Desc 1',
+"rowId": `row1`
+}
+]
+},
+{
+"title": `Seccion 2`,
+"rows": [
+{
+"title": 'Lista 2',
+"description": 'Desc 2',
+"rowId": `row 2`
+}
+]    
+}
+]
+}
+}, {quoted: sam})
+client.relayWAMessage(lista)          
+break
+                
+}
+
+} catch (e) {
+        
+console.log(e)}
+        
+})      
+}
 iniciar ()
 .catch (err => console.log("unexpected error: " + err))
